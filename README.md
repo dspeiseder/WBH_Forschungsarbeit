eff831e9e9fcfbe686d42bd334321a2545cbc45e
dspeiseder <dominik.speiseder@t-online.de>
2025-12-11 21:56:59 +0100

    Implemented run configuration as batch file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# to start quarkus with postgres db:
+```shell script
+./start-dev.ps1
+```
+
+
 # code-with-quarkus
 
 This project uses Quarkus, the Supersonic Subatomic Java Framework.
---
+++ start-dev.ps1
@@ -0,0 +1,29 @@
+$ErrorActionPreference = "Stop"
+
+# Start Postgres container in the background
+docker compose up -d
+
+# Wait until the DB is healthy (up to 60s)
+$timeoutSeconds = 60
+$elapsed = 0
+while ($true) {
+    $status = docker inspect --format '{{.State.Health.Status}}' itemsdb 2>$null
+
+    if ($status -eq "healthy") {
+        break
+    }
+
+    if ($status -eq "unhealthy") {
+        throw "Container itemsdb is unhealthy"
+    }
+
+    Start-Sleep -Seconds 2
+    $elapsed += 2
+
+    if ($elapsed -ge $timeoutSeconds) {
+        throw "Timed out waiting for Postgres to become healthy"
+    }
+}
+
+Write-Host "Postgres is healthy. Starting Quarkus dev mode..."
+./mvnw quarkus:dev
